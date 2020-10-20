c1369bc7de330b320bdc66eddcbfc517e6dc2201
Radfordhound <Grahamlrscott@gmail.com>
2020-10-19 23:46:08 -0500

    Expose more IO functions in call API

--- include/cglm/call/io.h
+++ include/cglm/call/io.h
@@ -26,6 +26,11 @@ glmc_mat3_print(mat3 matrix,
 
 CGLM_EXPORT
 void
+glmc_mat2_print(mat2 matrix,
+                FILE * __restrict ostream);
+
+CGLM_EXPORT
+void
 glmc_vec4_print(vec4 vec,
                 FILE * __restrict ostream);
 
@@ -36,8 +41,24 @@ glmc_vec3_print(vec3 vec,
 
 CGLM_EXPORT
 void
+glmc_ivec3_print(ivec3 vec,
+                 FILE * __restrict ostream);
+
+CGLM_EXPORT
+void
+glmc_vec2_print(vec2 vec,
+                FILE * __restrict ostream);
+
+CGLM_EXPORT
+void
 glmc_versor_print(versor vec,
                   FILE * __restrict ostream);
+
+CGLM_EXPORT
+void
+glmc_aabb_print(vec3 bbox[2],
+                const char * __restrict tag,
+                FILE * __restrict ostream);
 
 #ifdef __cplusplus
 }
