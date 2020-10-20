38d94c155a1f747e6ef7ee7d7e2320e3db76929b
Radfordhound <Grahamlrscott@gmail.com>
2020-10-19 23:26:43 -0500

    Add call API for color

---
+++ src/color.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#include "../include/cglm/cglm.h"
+#include "../include/cglm/call.h"
+
+CGLM_EXPORT
+float
+glmc_luminance(vec3 rgb) {
+  glm_luminance(rgb);
+}
